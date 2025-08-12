57a2a4f586ed29309700ccc8e76a17f08252ec22
muhamedhamedvl <mh1191128@gmail.com>
2025-08-12 20:18:08 +0300

    Added CourseVM

---
+++ Task1/Models/ViewModel/CourseVM.cs
@@ -0,0 +1,6 @@
+﻿using System.Xml.Linq;
+
+namespace Task1.Models.ViewModel
+{
+    public record CourseVM(string Name ,string Description ,string Category ,DateTime StartDate ,DateTime EndDate , int InstructorId);
+}
