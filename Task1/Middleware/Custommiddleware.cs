146958f005169e8478a67eeacf29b857bd61c02f
muhamedhamedvl <mh1191128@gmail.com>
2025-08-12 20:16:17 +0300

    CustomMiddleware

---
+++ Task1/Middleware/Custommiddleware.cs
@@ -0,0 +1,27 @@
+﻿namespace Task1.Middleware
+{
+    public class MyCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public MyCustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"[Middleware] Request: {context.Request.Method} {context.Request.Path}");
+            await _next(context);
+            Console.WriteLine($"[Middleware] Response Status Code: {context.Response.StatusCode}");
+        }
+    }
+
+    public static class MyCustomMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMyCustomMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyCustomMiddleware>();
+        }
+    }
+}
