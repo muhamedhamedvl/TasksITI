c07266f5371c4e4e1ac68866889ccaffb4eecb7b
muhamedhamedvl <mh1191128@gmail.com>
2025-08-12 20:18:27 +0300

    inject Custom Middleware in program.cs

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,5 @@
+using Task1.Middleware;
+
 namespace Task1
 {
     public class Program
@@ -6,29 +8,27 @@ namespace Task1
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddAuthorization(); 
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseAuthorization(); 
 
-            app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
